8bce26d554753721151d4ca9c38344b615e6a0c6
Kelvin Mai <kelvin.mai002@gmail.com>
2020-09-16 18:16:19 -0700

    bug fixes

--- apps/client/pages/launch/[id].tsx
+++ apps/client/pages/launch/[id].tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useGetLaunchAndMeQuery } from '@space-explorer/graphql/react';
+import { useGetLaunchAndMeLazyQuery } from '@space-explorer/graphql/react';
 
 import { Layout, UserHeader } from '../../components/layout';
 import { LaunchDetails } from '../../components/launch';
@@ -8,18 +8,22 @@ import { Loader } from '../../components/common';
 
 export const Launch = () => {
   const router = useRouter();
-  const { data, loading } = useGetLaunchAndMeQuery({
-    variables: { id: router.query.id as string },
-  });
+  const { id } = router.query;
+  const [query, { data, loading }] = useGetLaunchAndMeLazyQuery();
+  useEffect(() => {
+    if (id && typeof id === 'string') {
+      query({ variables: { id } });
+    }
+  }, [id]);
   return (
     <Layout title="Launch Details">
-      {loading ? (
-        <Loader />
-      ) : (
+      {!loading && data ? (
         <>
           <UserHeader email={data.me?.email}>Space Explorer</UserHeader>
           <LaunchDetails {...data.launch} />
         </>
+      ) : (
+        <Loader />
       )}
     </Layout>
   );
